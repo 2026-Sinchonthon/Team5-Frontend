06f202f6bac2bf958d4bb303a186eae04cde0ba6
unknown <gywjd2011316@gmail.com>
2026-08-29 16:55:11 +0900

    merge: router_index

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,27 +1,50 @@
 import { createBrowserRouter } from "react-router-dom";
-import RoleSelectPage from "../pages/auth/RoleSelectPage";
-import OwnerSplashPage from "../pages/auth/OwnerSplashPage";
-import OwnerLoginPage from "../pages/auth/OwnerLoginPage";
-import OwnerSignupPage from "../pages/auth/OwnerSignupPage";
+import Owner_RoleSelectPage from "../pages/auth/Owner_RoleSelectPage";
+import SplashPage from "../pages/auth/SplashPage";
+import Owner_LoginPage from "../pages/auth/Owner_LoginPage";
+import Owner_SignupPage from "../pages/auth/Owner_SignupPage";
 import { studentRoutes } from "./studentRoutes";
+
 import OwnerLayout from "../layouts/OwnerLayout";
+import OwnerHomePage from "../pages/owner/HomePage";
+import OwnerJobsPage from "../pages/owner/JobsPage";
+import JobsSearchPage from "../pages/owner/JobsSearchPage";
 import JobCreatePage from "../pages/owner/JobCreatePage";
+import JobProcessingPage from "../pages/owner/JobProcessingPage";
+import JobEditPage from "../pages/owner/JobEditPage";
+import JobCompletedPage from "../pages/owner/JobCompletedPage";
 import ApplicantListPage from "../pages/owner/ApplicantListPage";
+import OwnerMatchingPage from "../pages/owner/MatchingPage";
+import OwnerMyPage from "../pages/owner/MyPage";
+import AccountPage from "../pages/owner/AccountPage";
+import MyPostsPage from "../pages/owner/MyPostsPage";
 import OwnerChatPage from "../pages/owner/ChatPage";
 
 export const router = createBrowserRouter([
-  { path: "/", element: <RoleSelectPage /> },
-  { path: "/splash/owner", element: <OwnerSplashPage /> },
-  { path: "/login/owner", element: <OwnerLoginPage /> },
-  { path: "/signup/owner", element: <OwnerSignupPage /> },
+  { path: "/", element: <Owner_RoleSelectPage /> },
+  { path: "/splash/:role", element: <SplashPage /> },
+  { path: "/login/:role", element: <Owner_LoginPage /> },
+  { path: "/signup/:role", element: <Owner_SignupPage /> },
+
   ...studentRoutes,
+
+  { path: "/owner/jobs/search", element: <JobsSearchPage /> },
+  { path: "/owner/jobs/new", element: <JobCreatePage /> },
+  { path: "/owner/jobs/new/processing", element: <JobProcessingPage /> },
+  { path: "/owner/jobs/:jobId/edit", element: <JobEditPage /> },
+  { path: "/owner/jobs/:jobId/completed", element: <JobCompletedPage /> },
+  { path: "/owner/mypage/account", element: <AccountPage /> },
+  { path: "/owner/chat/:roomId", element: <OwnerChatPage /> },
   {
     path: "/owner",
     element: <OwnerLayout />,
     children: [
-      { path: "jobs/new", element: <JobCreatePage /> },
+      { path: "home", element: <OwnerHomePage /> },
+      { path: "jobs", element: <OwnerJobsPage /> },
       { path: "jobs/:jobId/applicants", element: <ApplicantListPage /> },
-      { path: "chat/:roomId", element: <OwnerChatPage /> },
+      { path: "matching", element: <OwnerMatchingPage /> },
+      { path: "mypage", element: <OwnerMyPage /> },
+      { path: "mypage/posts", element: <MyPostsPage /> },
     ],
   },
 ]);
